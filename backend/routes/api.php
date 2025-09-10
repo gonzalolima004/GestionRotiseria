24752926b771566d1a002096792cbd4756a75e59
Marian-Dominici <dominicimarianela@gmail.com>
2025-09-10 13:00:19 -0300

    admin

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdministradorController;
 
 Route::apiResource('/categorias', App\Http\Controllers\CategoriaController::class);
 Route::apiResource('/productos', App\Http\Controllers\ProductoController::class);
@@ -10,3 +10,6 @@ Route::apiResource('/ventas', App\Http\Controllers\VentaController::class);
 Route::apiResource('/pedidos', App\Http\Controllers\PedidoController::class);
 Route::apiResource('/detalle_pedidos', App\Http\Controllers\DetallePedidoController::class);
 
+Route::get('/administrador', [AdministradorController::class, 'show']);
+Route::put('/administrador', [AdministradorController::class, 'update']);
+Route::delete('/administrador', [AdministradorController::class, 'destroy']);
